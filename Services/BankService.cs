25b01c1ee01c120b546b1f0bfdc7df766faca823
patric1304 <patric.3pop@gmail.com>
2025-10-23 11:11:47 +0300

    Removed the files from teh old implementatino

--- Services/BankService.cs
+++ Services/BankService.cs
@@ -1,48 +1,179 @@
 namespace BankingApp.Services;
 
 using System;
-using BankingApp.Models;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using BankingApp.Models;
 
 public class BankService
 {
-    private List<Account> accounts = new();
+    private List<Bank> _banks = new();
+    private readonly string _dataFile = "banks.json";
 
-    public void CreateAccount(string accountHolder, AccountType accountType, AccountCurrency accountCurrency, string iban)
+    public BankService()
     {
-        var account = new Account(accountHolder, accountType, accountCurrency, iban);
-        accounts.Add(account);
+        LoadData();
+    }
+
+    public List<Bank> GetAllBanks()
+    {
+        return _banks;
+    }
+
+    public Bank? GetBank(string name)
+    {
+        return _banks.FirstOrDefault(b => b.Name == name);
+    }
+
+    public void CreateBank(string name, string swift)
+    {
+        var bank = new Bank(name, swift);
+        _banks.Add(bank);
+        SaveData();
+    }
+
+    public void CreateAccount(string bankName, string accountHolder, AccountType accountType, AccountCurrency currency)
+    {
+        var bank = GetOrCreateBank(bankName);
+        var iban = GenerateIban(bank);
+        var account = new Account(accountHolder, accountType, currency, iban);
+        bank.OpenAccount(account);
+        SaveData();
     }
 
     public bool Deposit(string iban, decimal amount)
     {
-        var account = accounts.Find(a => a.Iban == iban);
+        var account = FindAccount(iban);
         if (account == null) return false;
+        
         account.Deposit(amount);
+        SaveData();
         return true;
     }
 
     public bool Withdraw(string iban, decimal amount)
     {
-        var account = accounts.Find(a => a.Iban == iban);
+        var account = FindAccount(iban);
         if (account == null) return false;
-        return account.Withdraw(amount);
-    }
-
-    public Account? GetAccount(string iban)
-    {
-        return accounts.Find(a => a.Iban == iban);
-    }
-
-    public List<Account> GetAllAccounts()
-    {
-        return accounts;
+        
+        bool success = account.Withdraw(amount);
+        if (success) SaveData();
+        return success;
     }
 
     public bool ChangeCurrency(string iban, AccountCurrency newCurrency, Func<AccountCurrency, AccountCurrency, decimal> rateProvider)
     {
-        var account = accounts.Find(a => a.Iban == iban);
+        var account = FindAccount(iban);
         if (account == null) return false;
-        return account.ChangeCurrency(newCurrency, rateProvider);
+        
+        bool success = account.ChangeCurrency(newCurrency, rateProvider);
+        if (success) SaveData();
+        return success;
+    }
+
+    public bool DeleteAccount(string iban)
+    {
+        foreach (var bank in _banks)
+        {
+            if (bank.CloseAccount(iban))
+            {
+                SaveData();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Account? GetAccount(string iban)
+    {
+        return FindAccount(iban);
+    }
+
+    public List<Account> GetAllAccounts()
+    {
+        return _banks.SelectMany(b => b.Accounts).ToList();
+    }
+
+    public int GetTotalAccountsCount()
+    {
+        return _banks.Sum(b => b.Accounts.Count);
+    }
+
+    public int GetTotalBanksCount()
+    {
+        return _banks.Count;
+    }
+
+    private Account? FindAccount(string iban)
+    {
+        return _banks.SelectMany(b => b.Accounts)
+                     .FirstOrDefault(a => a.Iban == iban);
+    }
+
+    private Bank GetOrCreateBank(string bankName)
+    {
+        var bank = GetBank(bankName);
+        if (bank == null)
+        {
+            string swift = GenerateSwift(bankName);
+            bank = new Bank(bankName, swift);
+            _banks.Add(bank);
+        }
+        return bank;
+    }
+
+    private string GenerateSwift(string bankName)
+    {
+        string code = bankName.Length >= 4 
+            ? bankName.Substring(0, 4).ToUpper() 
+            : bankName.ToUpper().PadRight(4, 'X');
+        return code + "ROBU";
+    }
+
+    private string GenerateIban(Bank bank)
+    {
+        var random = new Random();
+        var iban = new string[18];
+        iban[0] = "DE";
+        iban[1] = "44";
+        iban[2] = "4";
+        iban[3] = "4";
+        iban[4] = "4";
+        iban[5] = "4";
+        iban[6] = "4";
+        iban[7] = "4";
+        iban[8] = "4";
+        iban[9] = "4";
+        iban[10] = "4";
+        iban[11] = "4";
+        iban[12] = "4";
+        iban[13] = "4";
+        iban[14] = "4";
+        iban[15] = "4";
+        iban[16] = "4";
+        iban[17] = "4";
+        iban[18] = "4";
+        return string.Join("", iban);
+    }
+
+    private void LoadData()
+    {
+        if (File.Exists(_dataFile))
+        {
+            var json = File.ReadAllText(_dataFile);
+            var banks = JsonSerializer.Deserialize<List<Bank>>(json);
+            if (banks != null)
+            {
+                _banks = banks;
+            }
+        }
+    }
+
+    private void SaveData()
+    {
+        var json = JsonSerializer.Serialize(_banks);
+        File.WriteAllText(_dataFile, json);
     }
 }
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,13 +1,214 @@
 namespace BankingApp.ViewModels;
 
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using BankingApp.Helpers;
+using BankingApp.Models;
+using BankingApp.Services;
 
 public class MainViewModel : BaseViewModel
 {
-    public ObservableCollection<object> Accounts { get; set; }
+    private readonly BankService _bankService;
+    private bool _isAccountsVisible;
+
+    public ObservableCollection<Account> Accounts { get; set; }
+    
+    public int TotalAccounts => _bankService.GetTotalAccountsCount();
+    public int TotalBanks => _bankService.GetTotalBanksCount();
+    
+    public bool IsAccountsVisible
+    {
+        get => _isAccountsVisible;
+        set
+        {
+            _isAccountsVisible = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(IsWelcomePanelVisible));
+        }
+    }
+
+    public bool IsWelcomePanelVisible => !_isAccountsVisible;
+
+    public ICommand ViewAccountsCommand { get; }
+    public ICommand CreateAccountCommand { get; }
+    public ICommand DepositCommand { get; }
+    public ICommand WithdrawCommand { get; }
+    public ICommand ChangeCurrencyCommand { get; }
+    public ICommand ViewTransactionsCommand { get; }
+    public ICommand DeleteAccountCommand { get; }
 
     public MainViewModel()
     {
-        Accounts = new ObservableCollection<object>();
+        _bankService = new BankService();
+        Accounts = new ObservableCollection<Account>();
+        
+        LoadAccounts();
+        
+        ViewAccountsCommand = new RelayCommand(ToggleAccountsView);
+        CreateAccountCommand = new RelayCommand(CreateAccount);
+        DepositCommand = new RelayCommand(Deposit, CanExecuteAccountAction);
+        WithdrawCommand = new RelayCommand(Withdraw, CanExecuteAccountAction);
+        ChangeCurrencyCommand = new RelayCommand(ChangeCurrency, CanExecuteAccountAction);
+        ViewTransactionsCommand = new RelayCommand(ViewTransactions, CanExecuteAccountAction);
+        DeleteAccountCommand = new RelayCommand(DeleteAccount, CanExecuteAccountAction);
+        
+        IsAccountsVisible = false;
+    }
+
+    private void ToggleAccountsView(object parameter)
+    {
+        IsAccountsVisible = !IsAccountsVisible;
+    }
+
+    private void CreateAccount(object parameter)
+    {
+        // Pass the list of banks to the dialog constructor
+        var dialog = new Views.AddAccountDialog(_bankService.GetAllBanks());
+        if (dialog.ShowDialog() == true)
+        {
+            _bankService.CreateAccount(
+                dialog.SelectedBankName,
+                dialog.AccountHolder,
+                dialog.AccountType,
+                dialog.AccountCurrency
+            );
+            RefreshAccounts();
+        }
+    }
+
+    private bool CanExecuteAccountAction(object parameter)
+    {
+        return parameter is Account;
+    }
+
+    private void Deposit(object parameter)
+    {
+        if (parameter is not Account account) return;
+
+        var dialog = new Views.AmountDialog("Deposit");
+        if (dialog.ShowDialog() == true)
+        {
+            if (_bankService.Deposit(account.Iban, dialog.Amount))
+            {
+                RefreshAccounts();
+                MessageBox.Show($"Successfully deposited {dialog.Amount} {account.AccountCurrency}",
+                    "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+
+    private void Withdraw(object parameter)
+    {
+        if (parameter is not Account account) return;
+
+        var dialog = new Views.AmountDialog("Withdraw");
+        if (dialog.ShowDialog() == true)
+        {
+            if (_bankService.Withdraw(account.Iban, dialog.Amount))
+            {
+                RefreshAccounts();
+                MessageBox.Show($"Successfully withdrew {dialog.Amount} {account.AccountCurrency}",
+                    "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Insufficient funds!", "Error", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+    private void ChangeCurrency(object parameter)
+    {
+        if (parameter is not Account account) return;
+
+        // CurrencyDialog doesn't take parameters - create it without arguments
+        var dialog = new Views.CurrencyDialog();
+        if (dialog.ShowDialog() == true)
+        {
+            if (_bankService.ChangeCurrency(account.Iban, dialog.SelectedCurrency, GetConversionRate))
+            {
+                RefreshAccounts();
+                MessageBox.Show($"Currency changed to {dialog.SelectedCurrency}",
+                    "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Insufficient funds for currency change!",
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+    private void ViewTransactions(object parameter)
+    {
+        if (parameter is not Account account) return;
+
+        var window = new Views.TransactionWindow(account);
+        window.ShowDialog();
+    }
+
+    private void DeleteAccount(object parameter)
+    {
+        if (parameter is not Account account) return;
+
+        var result = MessageBox.Show(
+            $"Are you sure you want to delete account {account.Iban}?",
+            "Confirm Delete",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Warning
+        );
+
+        if (result == MessageBoxResult.Yes)
+        {
+            if (_bankService.DeleteAccount(account.Iban))
+            {
+                RefreshAccounts();
+                MessageBox.Show("Account deleted successfully!", "Success",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+
+    private void LoadAccounts()
+    {
+        Accounts.Clear();
+        foreach (var account in _bankService.GetAllAccounts())
+        {
+            Accounts.Add(account);
+        }
+    }
+
+    private void RefreshAccounts()
+    {
+        LoadAccounts();
+        OnPropertyChanged(nameof(TotalAccounts));
+        OnPropertyChanged(nameof(TotalBanks));
+    }
+
+    private decimal GetConversionRate(AccountCurrency from, AccountCurrency to)
+    {
+        if (from == to) return 1m;
+
+        var rates = new System.Collections.Generic.Dictionary<(AccountCurrency, AccountCurrency), decimal>
+        {
+            { (AccountCurrency.RON, AccountCurrency.EUR), 0.20m },
+            { (AccountCurrency.EUR, AccountCurrency.RON), 5.0m },
+            { (AccountCurrency.USD, AccountCurrency.EUR), 0.92m },
+            { (AccountCurrency.EUR, AccountCurrency.USD), 1.09m },
+            { (AccountCurrency.GBP, AccountCurrency.EUR), 1.17m },
+            { (AccountCurrency.EUR, AccountCurrency.GBP), 0.85m },
+            { (AccountCurrency.USD, AccountCurrency.RON), 4.6m },
+            { (AccountCurrency.RON, AccountCurrency.USD), 0.22m },
+            { (AccountCurrency.GBP, AccountCurrency.RON), 5.85m },
+            { (AccountCurrency.RON, AccountCurrency.GBP), 0.17m },
+            { (AccountCurrency.USD, AccountCurrency.GBP), 0.78m },
+            { (AccountCurrency.GBP, AccountCurrency.USD), 1.28m }
+        };
+
+        return rates.TryGetValue((from, to), out var rate) ? rate : 1m;
     }
 }
